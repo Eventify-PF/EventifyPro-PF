ced6a804a4e3dab8f6f9127d9efcfe856d79df96
FVidalYepz <Frananyelvidalyepez@gmail.com>
2023-11-01 18:19:03 -0400

    Optimización

--- api/src/controllers/modifyEvent.js
+++ api/src/controllers/modifyEvent.js
@@ -11,29 +11,24 @@ const modifyEvent = async (
   EventTypeId
 ) => {
   try {
-    let updatedEvent = await Events.findOne({ where: { id: id } });
-    let newEventTypeId = await EventTypes.findOne({
+    const updatedEvent = await Events.findOne({ where: { id } });
+    const newEventTypeId = await EventTypes.findOne({
       where: { id: EventTypeId },
     });
-    let updates = {};
-    if (title !== undefined) updates.title = title;
-    if (image !== undefined) updates.image = image;
-    if (date !== undefined) updates.date = date;
-    if (location !== undefined) updates.location = location;
-    if (description !== undefined) updates.description = description;
-    if (status !== undefined) updates.status = status;
-    if (status !== undefined)
-      updates.EventTypeId = newEventTypeId.dataValues.id;
 
-    await updatedEvent.update({
-      title,
-      image,
-      date,
-      location,
-      description,
-      status,
-      EventTypeId,
-    });
+    const updates = {
+      ...(title !== undefined && { title }),
+      ...(image !== undefined && { image }),
+      ...(date !== undefined && { date }),
+      ...(location !== undefined && { location }),
+      ...(description !== undefined && { description }),
+      ...(status !== undefined && { status }),
+      ...(newEventTypeId !== undefined && {
+        EventTypeId: newEventTypeId.dataValues.id,
+      }),
+    };
+
+    await updatedEvent.update(updates);
 
     return updatedEvent;
   } catch (error) {
